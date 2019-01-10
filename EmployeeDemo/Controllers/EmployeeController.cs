0896909163f64346115635a528d445172c8fcc4a
Ankita <ankita.m@softwaysolutions.com>
2019-01-10 11:03:25 +0530

    MVC practice program 1 on list of employees

---
+++ EmployeeDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDemo.Models;
+
+
+namespace EmployeeDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            List<Employee> model = new List<Employee>()
+               {
+                   new Employee{
+                       EmpName = "ankita",
+                       EmpId = 001,
+                       Emp_Email = "ankita@gmail.com",
+                       Gender = " female",
+                       Emp_City = " belagavi"
+                   },
+                   new Employee{
+                       EmpName = "anisha",
+                       EmpId = 002,
+                       Emp_Email = "anisha@gmail.com",
+                       Gender = " female",
+                       Emp_City = " belagavi"
+                   },
+                   
+               };
+            return View(model);
+
+        }
+    }
+}
---
+++ EmployeeDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDemo.Models
+{
+    public class Employee
+    {
+        public string EmpName { get; set; }
+        public int EmpId { get; set; }
+        public string Emp_Email { get; set; }
+        public string Gender { get; set; }
+        public string Emp_City { get; set; }
+    }
+}
